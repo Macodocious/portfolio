384a1430a8b8fdd0074169fcaf703a5052be0c76
Brian Mac <brian.mac@tooni.money>
2024-06-11 17:59:40 -0400

    Fixed Gulp (it only watched HTML)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.html',
-    './src/**/*.js',
+    './src/**/*.{html,js}',
     './index.html',
   ],
   theme: {
